972ae9abf47ececd4b7500134c1448f23c3bc32b
Christian Kaarre <christiankaarre@MacBook-Christian.local>
2020-05-07 17:34:56 +0300

    Improved comments

--- VisualTrajectory/ViewController.swift
+++ VisualTrajectory/ViewController.swift
@@ -17,18 +17,29 @@ class ViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
     
-    var startPoint = [Float(0), Float(0), Float(0)]
-    var sceneNodes = [SCNNode]()
+    // PROPERTIES
     
+    // point for calculating the Euclidian distance,
+    // updated after distanceOfImages has been reached
+    var startPoint = [Float(0), Float(0), Float(0)]
+    // array of the nodes in the scene
+    var sceneNodes = [SCNNode]()
+    // x, y and z coordinates for timer
     var xPoint = Float(0)
     var yPoint = Float(0)
     var zPoint = Float(0)
     
+    // name of the data-file
     var file = ""
-    
+    // UILabel properties
     let label = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 21))
     
     
+    
+    /* viewDidLoad
+     1. Timer initialized
+     2. UILabel initialized
+     */
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -38,7 +49,7 @@ class ViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate {
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
         
-        
+        // Show the featurepoints
         //sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
         
         var timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(timerAction), userInfo: nil, repeats: true)
@@ -50,6 +61,9 @@ class ViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate {
     }
     
     
+    /* Timer Action
+     writes the current location 3DoF into a .txt-file
+     */
     @objc func timerAction() {
         let point = String(xPoint) + " " + String(yPoint) + " " + String(zPoint) + "\n"
         
@@ -64,6 +78,12 @@ class ViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate {
     }
     
 
+    /* ARSession's 'didUpdate' delegate-method
+     Handles the ARFrame objects
+     1. sets the UILabel text
+     2. calculates the Euclidean distance between specified points
+        1. calls addNodesToScene()
+    */
     func session(_ session: ARSession, didUpdate frame: ARFrame) {
         
         xPoint = Float(frame.camera.transform.columns.3[0])
@@ -78,38 +98,39 @@ class ViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate {
             pow(yPoint - startPoint[1], 2) +
             pow(zPoint - startPoint[2], 2)
 
-        if ( sqrt(distancePower) >= 1.0 ) {
+        if ( sqrt(distancePower) >= 0.3 ) {
             addNodeToScene(xPoint: xPoint, yPoint: yPoint, zPoint: zPoint)
             startPoint = [xPoint, yPoint, zPoint]
         }
     }
 
     
+    /* Creates ball-node based on the 3DoF coordinates
+     and adds the node into rootNode
+     */
     func addNodeToScene(xPoint: Float, yPoint: Float, zPoint: Float) {
         
-        let ball = SCNSphere(radius: 0.05)
+        let ball = SCNSphere(radius: 0.015)
         let material = SCNMaterial()
         material.diffuse.contents = UIColor.red
         ball.materials = [material]
         
         let node = SCNNode()
-        node.position = SCNVector3(x: xPoint, y: yPoint-0.3, z: zPoint)
+        node.position = SCNVector3(x: xPoint, y: yPoint-0.1, z: zPoint)
         node.geometry = ball
         sceneView.scene.rootNode.addChildNode(node)
         sceneView.autoenablesDefaultLighting = true
     }
     
     
+    /* viewWillAppear
+     1. creates a .txt-file for camera parameters
+     2. defines the session's configuration as ARWorldTrackingConfiguration
+        and sets the plane detection
+     3. runs the session
+     */
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        
-        // Create a session configuration
-        let configuration = ARWorldTrackingConfiguration()
-        configuration.planeDetection = [.horizontal, .vertical]
-        
-        // Run the view's session
-        sceneView.session.run(configuration)
-        sceneView.session.delegate = self
         
         // Initialize the file
         let today = Date()
@@ -128,9 +149,19 @@ class ViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate {
                 print(error)
             }
         }
+        
+        // Create a session configuration
+        let configuration = ARWorldTrackingConfiguration()
+        configuration.planeDetection = [.horizontal, .vertical]
+        
+        // Run the view's session
+        sceneView.session.run(configuration)
+        sceneView.session.delegate = self
     }
     
-    
+  
+    /* viewWillDisappear
+     */
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         
